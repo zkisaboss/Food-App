e45bd01d1fb2834811d1e77e89201ddbf7a498a6
zkisaboss <51681731+zkisaboss@users.noreply.github.com>
2023-02-27 12:25:56 -0500

    STILL BROKEN

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class AccountManager:
         return username
 
     @staticmethod
-    def login():  # sourcery skip: use-contextlib-suppress
+    def login():
         for _ in range(3):
             username = input("Enter your username: ")
             password = input("Enter your password: ")
@@ -30,7 +30,7 @@ class AccountManager:
                 if account[username] == password:
                     return username
             except (FileNotFoundError, KeyError):
-                pass
+                print("Could not load profile.")
 
             print("Invalid username/password combination.")
 
@@ -43,20 +43,16 @@ class AccountManager:
                 "Enter '1' to create a new account or '2' to login to an existing one: ")
 
             if choice == '1':
-                AccountManager.signup()
-                break
-
+                return AccountManager.signup()
             elif choice == '2':
-                AccountManager.login()
-                break
-
+                return AccountManager.login()
             else:
                 print("Invalid choice. Please enter '1' or '2'.")
 
 
 ### MISSING LOGIC ###
 class DataCollector:
-    def init(self):
+    def __init__(self):
         self.foods = [
             "pizza",
             "chicken",
@@ -86,9 +82,9 @@ class DataCollector:
         first_food = random.choice(self.foods)
 
         for _ in range(min(14, len(self.foods) - 1)):
-            second_food = random.choice([
-                food for food in self.foods if food != first_food
-            ])
+            second_food = random.choice(
+                [food for food in self.foods if food != first_food]
+            )
             choice = self.preference(first_food, second_food)
 
             if choice == 2:
@@ -103,10 +99,15 @@ class DataCollector:
 
 ####### MESSY #######
 class DataManager:
-    @staticmethod
-    def store_choices(choice_data):
+
+    def __init__(self, choice_data):
+        self.choice_data = choice_data
+
+
+    def store_choices(self):
         local_dict = {}
-        for choice_tuple in choice_data:
+        self.foods = DataCollector().foods
+        for choice_tuple in self.choice_data:
             food1, food2 = choice_tuple
 
             if food1 in local_dict:
@@ -115,20 +116,25 @@ class DataManager:
                 local_dict[food1] = (local_dict.get(food2, 2) + 1) // 2
 
             if food2 not in local_dict:
-                DataCollector.foods.append(food2)
+                data_collector.foods.append(food2)
 
-        return local_dict
+        return list(local_dict.items())
 
     @staticmethod
     def update_global_from_local(local_dict, global_dict):
-        for food, count in local_dict.items():
-            global_dict[food] = global_dict.get(food, 0) + count
+        for food, count in local_dict:
+
+            # global_dict[food] = global_dict.get(food, 0) + count
+            if food in global_dict:
+                global_dict[food] += count
+            else:
+                global_dict[food] = count
 
         return global_dict
 
-    @staticmethod
-    def gather_and_update_choices(choice_data, global_dict):
-        local_dict = DataManager.store_choices(choice_data)
+
+    def gather_and_update_choices(self, global_dict):
+        local_dict = DataManager.store_choices(self)
         DataManager.update_global_from_local(local_dict, global_dict)
         return dict(sorted(global_dict.items(), key=lambda item: item[1], reverse=True))
 
@@ -161,13 +167,16 @@ if __name__ == '__main__':
         global_dict = json.load(f)["global_dict"]
 
     data_collector = DataCollector()
-    choice_data = data_collector.gather_preferences(username)
+    choice_data = data_collector.gather_preferences()
 
-    data_manager = DataManager()
+    data_manager = DataManager(choice_data)
+
     others = Others()
+
     while True:
         global_dict = data_manager.gather_and_update_choices(
-            choice_data, global_dict)
+            choice_data)
+
         global_pct_dict = data_manager.val_to_pct(global_dict)
 
         print(data_collector.foods)
