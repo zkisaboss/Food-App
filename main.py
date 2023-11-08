b90c57beed7c87497e299648f114c7c249776fcc
Zachary Kosove <51681731+zkisaboss@users.noreply.github.com>
2023-11-08 13:45:48 -0500

    Update main.py
    Improved initialization of new accounts.
    Sign Up section terminates after three failed attempts.
    Unseen items are now included in recommendations.
    Bug Fixes Regarding the DataHandler

--- main.py
+++ main.py
@@ -2,55 +2,54 @@ import json
 import os
 import random
 
-
-"""
-To-Do:
-- Check for scalability
-
-Planned Qualities:
-- Adjusts Suggestions Based on Current Session Interactions.
-    - Items that are frequently purchased together?
-    - Items that are categorically similar?
-    - Items that you've picked in the past?
-
-- Avoids Information Confinement Area by Periodically Introduce New Items.
-- Adjusts for Changing Trends.
-"""
+# Implement tensorflow recommendations instead of static methods
+# Add a limitation that blocks recommendations if the food is not found in a restaurant's menu within a certain range of the user's geo-location
+foods = ["sushi", "grilled salmon", "steak", "tacos", "hamburger", "waffles", "noodles", "barbecue ribs", "pizza",
+         "calamari", "pulled pork", "chicken", "dumplings", "rice", "tandoori chicken", "soup", "example2"]
 
 
 class AccountManager:
-    """
-    Takes string inputs: username and password.
-    Returns a variable containing the username: USER.
-    """
-
     @staticmethod
-    def signup():
-        while True:
+    def signup() -> str:
+        for _ in range(3):
             USER = input("Enter a username: ")
             if os.path.exists(f"Profiles/{USER}.json"):
-                print("Username already exists. Please try a different username.")
+                print("Error: Username already exists.")
             else:
                 break
+        else:
+            print("Raising SystemExit")
+            raise SystemExit
 
         PASS = input("Enter a password: ")
-        account = {USER: PASS, "clicks": {}, "impressions": {}, "cpi": {}}
+        account = {USER: PASS,
+                   "clicks": {food: 0 for food in foods},
+                   "impressions": {food: 0 for food in foods},
+                   "cpi": {food: 0 for food in foods}}
 
-        with open(f"Profiles/{USER}.json", "w") as f:
-            json.dump(account, f, indent=4, separators=(',', ': '))
+        with open(f"Profiles/{USER}.json", "w") as fn:
+            json.dump(account, fn, indent=4, separators=(',', ': '))
 
         print("Account created successfully!")
         return USER
 
     @staticmethod
-    def login():
+    def login() -> str:
         for _ in range(3):
             USER = input("Enter your username: ")
             PASS = input("Enter your password: ")
 
             try:
-                with open(f"Profiles/{USER}.json", "r") as f:
-                    account = json.load(f)
+                with open(f"Profiles/{USER}.json", "r") as fn:
+                    account = json.load(fn)
+
+                unique = account['impressions'].keys() ^ foods  # Checks for food items not in the user json file
+                for i in unique:
+                    if i in foods:
+                        account['impressions'][i] = 0
+
+                with open(f"Profiles/{USER}.json", "w") as fn:
+                    json.dump(account, fn, indent=4)
 
                 if account[USER] == PASS:
                     return USER
@@ -80,11 +79,6 @@ class AccountManager:
 
 
 class DataCollector:
-    """
-    Takes integers: '1' and '2'.
-    Returns dictionaries: clicks, impressions.
-    """
-
     def __init__(self):
         self.clicks = {}
         self.impressions = {}
@@ -101,10 +95,9 @@ class DataCollector:
         self.impressions[a] = self.impressions.get(a, 0) + 1
         self.impressions[b] = self.impressions.get(b, 0) + 1
 
-    # Planned: Session-Based Recommendations
     def collect_data(self):
         a = self.recommendations[0]
-        
+
         for i in range(total_decisions):
             b = self.recommendations[i + 1]
             a, b = self.arrange(a, b)
@@ -115,11 +108,6 @@ class DataCollector:
 
 
 class DataHandler:
-    """
-    Takes dictionaries: clicks, impressions.
-    Updates the user's JSON file.
-    """
-
     def __init__(self, c, i):
         self.clicks = self.merge(c, user["clicks"])
         self.impressions = self.merge(i, user["impressions"])
@@ -133,8 +121,14 @@ class DataHandler:
         return dict(sorted(d2.items(), key=lambda item: item[1], reverse=True))
 
     @staticmethod
-    def modify_cpi(d1, d2):
-        d3 = {k: d1[k] / d2[k] * 100 for k in d1}
+    def modify_cpi(d1, d2):  # sourcery skip: assign-if-exp, dict-comprehension
+        d3 = {}
+        for k in d1:
+            if k in d2 and d2[k] != 0:
+                d3[k] = (d1[k] / d2[k] * 100)
+            else:
+                d3[k] = 0
+
         return dict(sorted(d3.items(), key=lambda item: item[1], reverse=True))
 
     def update(self, user):
@@ -142,16 +136,11 @@ class DataHandler:
         user["impressions"] = self.impressions
         user["cpi"] = self.modify_cpi(self.clicks, self.impressions)
 
-        with open(my_json, "w") as f:
-            json.dump(user, f, indent=4, separators=(',', ': '))
+        with open(my_json, "w") as fn:
+            json.dump(user, fn, indent=4, separators=(',', ': '))
 
 
 class NearestNeighbors:
-    """
-    Takes Current User's File: f'{USER}.file'.
-    Returns a list, which contains tuples: data.
-    """
-
     def __init__(self):
         self.seen_files = {f'{USER}.json'}
         self.directory = os.listdir('Profiles/')
@@ -166,6 +155,7 @@ class NearestNeighbors:
         key_difference_sum = sum(abs(d1[k] - d2[k]) for k in shared_keys)
         return round(key_difference_sum / len(shared_keys), 2)
 
+    # sep into two different functions for simplicity
     def get_unseen_profile(self):
         while True:
             _file = random.choice(self.directory)
@@ -173,34 +163,24 @@ class NearestNeighbors:
                 self.seen_files.add(_file)
                 break
 
-        with open(os.path.join('Profiles/', _file), 'r') as f:
-            dct = dict(json.load(f)["cpi"])
-        return _file, dct
+        with open(os.path.join('Profiles/', _file), 'r') as fn:
+            dct = dict(json.load(fn)["cpi"])
 
-    def get_nonempty_profile(self):
-        while not (profile := self.get_unseen_profile())[1]:
-            continue
-        return profile
+        return _file, dct
 
     @property
     def get(self):
         data = []
 
         for _ in self.num_neighbors:
-            username, json_data = self.get_nonempty_profile()
+            username, json_data = self.get_unseen_profile()
             similarity = self.calculate_difference(user["cpi"], json_data)
             data.append((similarity, username, json_data))
 
         return sorted(data, key=lambda x: x[0])
 
 
-# Planned: Periodically Introduce New/Unique Items.
 class RecommendationHandler:
-    """
-    Takes list of tuples: nearest.
-    Returns list of strings: recommendations.
-    """
-
     def __init__(self):
         self.seen_items = set()
         self.elements = total_decisions + 1
@@ -212,11 +192,20 @@ class RecommendationHandler:
         for d in dicts:
             for k, v in d.items():
                 result[k] = result.get(k, []) + [v]
-        return {k: sum(v)/len(v) for k, v in result.items()}
+        return {k: sum(v) / len(v) for k, v in result.items()}
 
     @staticmethod
-    def suggested_ele(dct):
-        return random.choice([k for k in dct for _ in range(int(dct[k]))])
+    def suggested_ele(dct, probability=50):
+        items = [k for k in dct for _ in range(int(dct[k]))]
+        unseen_items = [k for k in dct if dct[k] == 0]
+
+        # Calculate the probability for new items
+        unseen_probability = probability / len(items) if items else 0
+
+        # Append unseen items with dynamically increasing probabilities
+        items += [k for k in unseen_items for _ in range(int(unseen_probability * len(items)))]
+
+        return random.choice(items)
 
     @property
     def get(self):
