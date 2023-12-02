bdabecc877dd999510b69514c7b3e619b098b36e
Zachary Kosove <51681731+zkisaboss@users.noreply.github.com>
2023-12-02 17:55:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import json
 import os
 import random
 
+
 # Implement TensorFlow recommendations instead of static methods.
-nearby_foods = ["sushi", "grilled salmon", "steak", "tacos", "hamburger", "waffles", "noodles",
-                "pizza", "calamari", "pulled pork", "chicken", "dumplings", "rice", "tandoori chicken", "soup",
-                "burritos"]
+nearby_foods = {"avocado toast", "burritos", "chicken and rice", "chicken stir fry", "chicken wings", "crab legs", 
+                "crêpes", "dumplings", "french toast", "gnocchi", "grilled oysters", "grilled salmon", "hamburgers", 
+                "lobster tail", "pizza", "quesadillas", "ramen noodles", "shrimp scampi", "smoothies", "soup", 
+                "spaghetti & meatballs", "steak", "steamed clams", "sushi", "tacos", "waffles"}
 
 
 class AccountManager:
@@ -14,7 +16,7 @@ class AccountManager:
         for _ in range(3):
             USER = input("Enter a username: ")
             if os.path.exists(f"Profiles/{USER}.json"):
-                print("Error: Username already exists.")
+                print("Username already exists.")
             else:
                 break
         else:
@@ -37,40 +39,27 @@ class AccountManager:
             USER = input("Enter your username: ")
             PASS = input("Enter your password: ")
 
-            try:
-                with open(f"Profiles/{USER}.json") as file:
-                    account = json.load(file)
-
-                with open(f"Profiles/{USER}.json", "w") as file:
-                    unseen = set(nearby_foods) - set(account['impressions'])
-                    for i in unseen:
-                        account['clicks'][i] = 0
-                        account['impressions'][i] = 0
-                        account['cpi'][i] = 0
-                    json.dump(account, file, indent=4, separators=(',', ': '))
-
-                if account[USER] == PASS:
-                    return USER
-                else:
-                    print("Invalid username/password combination.")
-            except (FileNotFoundError, KeyError):
-                print("Could not load profile.")
-
+            file_path = f"Profiles/{USER}.json"
+            if os.path.exists(file_path) and json.load(open(file_path)).get(USER) == PASS:
+                return USER
+            else:
+                print("Incorrect username or password.")
 
         print("You've exceeded the number of login attempts.")
         raise SystemExit
 
     def interaction(self) -> str:
-        while True:
-            user_input = input(
-                "Enter '1' to create a new account or '2' to login to an existing one: ")
-
-            if user_input == '1':
+        for i in range(3):
+            var = input("Enter '1' to create an account or '2' to login to an existing one: ")
+            if var == '1':
                 return self.signup()
-            elif user_input == '2':
+            elif var == '2':
                 return self.login()
-            else:
+            elif i < 2:
                 print("Invalid choice. Please enter '1' or '2'.")
+
+        print("Raising SystemExit")
+        raise SystemExit
 
     @property
     def manage(self) -> str:
@@ -78,7 +67,7 @@ class AccountManager:
 
 
 class DataCollector:
-    def __init__(self, r):
+    def __init__(self, r: list):
         self.clicks = {}
         self.impressions = {}
         self.recommendations = r
@@ -86,8 +75,8 @@ class DataCollector:
 
     @staticmethod
     def arrange(a: str, b: str) -> tuple:
-        print(f"Do you prefer: {a} or {b}?")
-        return (a, b) if input("Enter 1 for the first option or 2 for the second: ") == "1" else (b, a)
+        print(f"\nDo you prefer: {a} or {b}?")
+        return (a, b) if input(f"Enter 1 for {a} or 2 for {b}: ") == "1" else (b, a)
 
     def store(self, a: str, b: str):
         self.clicks[a] = self.clicks.get(a, 0) + 1
@@ -96,7 +85,6 @@ class DataCollector:
 
     def collect_data(self):
         a = self.recommendations[0]
-
         for i in range(5):
             b = self.recommendations[i + 1]
             a, b = self.arrange(a, b)
@@ -110,7 +98,7 @@ class DataHandler:
     def __init__(self, c: dict, i: dict):
         self.clicks = self.merge(c, user_data["clicks"])
         self.impressions = self.merge(i, user_data["impressions"])
-        self.update()
+        self.store_changes()
 
     @staticmethod
     def merge(d1: dict, d2: dict) -> dict:
@@ -120,20 +108,20 @@ class DataHandler:
         return d2
 
     @staticmethod
-    def modify_cpi(d1: dict, d2: dict) -> dict:
+    def calculate_cpi(d1: dict, d2: dict) -> dict:
         d3 = {}
         for k in d1:
-            try:
-                d3[k] = d1[k] / d2[k] * 100
-            except (KeyError, ZeroDivisionError):
+            if d1[k] == 0 or d2[k] == 0:
                 d3[k] = 0
+            else:
+                d3[k] = d1[k] / d2[k] * 100
 
         return d3
 
-    def update(self):
-        user_data["clicks"] = self.clicks
-        user_data["impressions"] = self.impressions
-        user_data["cpi"] = self.modify_cpi(self.clicks, self.impressions)
+    def store_changes(self):
+        user_data.update(clicks=self.clicks,
+                         impressions=self.impressions,
+                         cpi=self.calculate_cpi(self.clicks, self.impressions))
 
         with open(f"Profiles/{username}.json", "w") as file:
             json.dump(user_data, file, indent=4, separators=(',', ': '))
@@ -154,23 +142,22 @@ class NearestNeighbors:
         key_difference_sum = sum(abs(d1[k] - d2[k]) for k in shared_keys)
         return key_difference_sum / len(shared_keys)
 
-    def get_unseen_profile(self) -> dict:
+    def retrieve_profile(self) -> dict:
         random_file = random.choice(self.directory)
         self.directory.remove(random_file)
 
         with open(f"Profiles/{random_file}") as file:
-            json_data = json.load(file)["cpi"]
+            json_data = json.load(file)
 
-        return json_data
+        return json_data["cpi"]
 
     @property
-    def get(self) -> list:
+    def fetch(self) -> list:
         dicts = []
         scores = []
         for _ in range(self.k_neighbors):
-            json_data = self.get_unseen_profile()
+            json_data = self.retrieve_profile()
             dicts.append(json_data)
-
             similarity = self.calculate_difference(json_data, user_data["cpi"])
             scores.append(similarity)
 
@@ -188,25 +175,31 @@ class RecommendationHandler:
         for d in dicts:
             for k, v in d.items():
                 result[k] = result.get(k, []) + [v]
+
         return {k: sum(v) / len(v) for k, v in result.items()}
 
     @staticmethod
-    def generate_suggestions(d1):
+    def calculate_values(d1: dict) -> tuple:
         seen = []
         unseen = []
         weights = []
         for k, v in d1.items():
-            if v:
+            if v != 0:
                 seen.append(k)
                 weights.append(v)
             else:
                 unseen.append(k)
+
         keys = seen + unseen
 
         if unseen:
             length = len(unseen)
-            weights.extend([sum(weights) / length * 0.1111111111111111] * length)
+            weights.extend([sum(weights) / length * 0.2222222222222222] * length)
 
+        return keys, weights
+
+    @staticmethod
+    def generate_suggestions(keys: list, weights: list) -> list:
         selected_keys = set()
         while len(selected_keys) < 6:
             selected_keys.add(random.choices(keys, weights)[0])
@@ -214,12 +207,13 @@ class RecommendationHandler:
         return list(selected_keys)
 
     @property
-    def get(self) -> list:
-        return self.generate_suggestions(self.nearest)
+    def fetch(self) -> list:
+        keys, weights = self.calculate_values(self.nearest)
+        return self.generate_suggestions(keys, weights)
 
 
 def proceed() -> int:
-    print("Do you want to proceed or retry?")
+    print("\nDo you want to proceed or retry?")
     return input("Enter 1 to Proceed or 2 to Retry: ") == "1"
 
 
@@ -231,15 +225,15 @@ if __name__ == '__main__':
             user_data = json.load(f)
 
         while True:
-            nearest = NearestNeighbors(username).get
-            recommendations = RecommendationHandler(nearest).get
-
+            nearest = NearestNeighbors(username).fetch
+            recommendations = RecommendationHandler(nearest).fetch
             clicks, impressions = DataCollector(recommendations)
             DataHandler(clicks, impressions)
 
             if proceed():
                 break
 
-        print("Loading Search Results...")
+        print("\nLoading Search Results...")
+
     except KeyboardInterrupt:
         raise SystemExit
